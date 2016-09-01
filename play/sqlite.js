29bf3678042ea86506c9939fcf69b1724c54bb3c
Christopher <chriscrowd@gmail.com>
2016-09-01 13:35:57 -0400

    added sqlite

---
+++ play/sqlite.js
@@ -0,0 +1,64 @@
+var Sequelize = require('sequelize');
+
+var sequelize = new Sequelize(undefined,undefined,undefined,{
+  'dialect':'sqlite',
+  'storage':__dirname + '/basic-sqlite-database.sqlite'
+});
+
+var Todo = sequelize.define('todo',{
+  description:{
+    type:Sequelize.STRING,
+    allowNull:false,
+    validate:{
+      len:[1,250]
+    }
+  },
+  completed:{
+    type:Sequelize.BOOLEAN,
+    allowNull:false,
+    defaultValue:false
+  }
+})
+
+sequelize.sync().then(function() {
+    console.log("Good!");
+     return Todo.findById(5)
+     .then(function(todo) {
+       if (!todo) {
+         console.log("No Record Found");
+         return;
+       }
+       console.log(todo.toJSON());
+     })
+ /*
+    Todo.create({
+      description:'Study Node',
+      completed:false
+    }).then(function(data) {
+         return Todo.create({
+           description:'walk my Coon Cat',
+           completed:true
+         }).then(function() {
+            //return Todo.findById(2);
+            return Todo.findAll({
+              where:{
+                description:{
+                  $like:'%cat%'
+                }
+              }
+            })
+         }).then(function(todos) {
+           if (todos) {
+             todos.forEach(function(todo) {
+                console.log(todo.toJSON());
+             });
+
+           }else {
+             console.log('no data');
+           }
+         })
+         */
+    .catch(function(error) {
+      console.log(error);
+    })
+})
